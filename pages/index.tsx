7a720b3329718f31e9020c29704e38a77ac29fb7
angelzxz1 <angel.zuna@icloud.com>
2022-12-14 09:08:15 -0500

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,60 @@ import { useState } from "react";
 import RepositorySearch from "../components/RepositorySearch";
 import OwnerRepositories from "../components/OwnerRepositories";
 const Home = () => {
-  return (
-    <Box>
-      <RepositorySearch />
-    </Box>
-  );
+    const [posT, setPostT] = useState(-1000);
+    const [posB, setPostB] = useState(-1000);
+    return (
+        <Grid
+            templateAreas={`"top" "bot"`}
+            templateColumns="1fr"
+            templateRows="1fr 1fr"
+            gap={5}
+            mt={5}
+        >
+            {/*  */}
+            <Button
+                gridArea="top"
+                onClick={() => {
+                    if (posT === -1000 && posB === -1000) {
+                        setPostT(0);
+                    } else {
+                        setPostT((prev) => (prev === -1000 ? 0 : -1000));
+                    }
+                }}
+            >
+                Looking for a repo?
+            </Button>
+            <Button
+                gridArea="bot"
+                onClick={() => {
+                    if (posT === -1000 && posB === -1000) {
+                        setPostB(0);
+                    } else {
+                        setPostB((prev) => (prev === -1000 ? 0 : -1000));
+                    }
+                }}
+            >
+                Looking for all repos of an account?
+            </Button>
+            <Box position="relative">
+                <Box
+                    position="absolute"
+                    top={`${posT}px`}
+                    w="100%"
+                    transition="top 300ms ease"
+                >
+                    <RepositorySearch />
+                </Box>
+                <Box
+                    position="absolute"
+                    top={`${posB}px`}
+                    w="100%"
+                    transition="top 300ms ease"
+                >
+                    asdasdas
+                </Box>
+            </Box>
+        </Grid>
+    );
 };
 export default Home;
